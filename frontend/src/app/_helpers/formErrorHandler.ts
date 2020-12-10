76abcba4525d0085a30b46ecc5a2fcdd49f8b0e6
Cass <dev@cass.si>
2020-12-10 15:28:39 +0000

    add tooling to make custom error handling easier

--- frontend/src/app/_helpers/formErrorHandler.ts
+++ frontend/src/app/_helpers/formErrorHandler.ts
@@ -1,19 +1,36 @@
-import { ICacheable } from '../app.interfaces';
-import { IErrorResponse } from '@eventi/interfaces';
+import { ICacheable } from "../app.interfaces";
+import { IErrorResponse, IFormErrorField } from "@eventi/interfaces";
+import { FormGroup } from "@angular/forms";
 
-export const handleFormErrors = (obj:ICacheable<any>, error:IErrorResponse):ICacheable<any> => {
-    // Objects pass by reference, stay pure please :)
-    const o = Object.assign({}, obj);
+export const handleFormErrors = (obj: ICacheable<any>, error: IErrorResponse): ICacheable<any> => {
+  // Objects pass by reference & reset fields, stay pure please :)
+  const o = Object.assign({}, obj);
+  o.error = null;
 
-    // Put general form error
-    o.error = error.message;
+  // Assign does shallow copy, but we want all props from prev obj
+  o.form_errors = Object.keys(o.form_errors).reduce(
+    (acc, curr) => ({ ...acc, [curr]: null }),
+    {}
+  );
 
-    // Map to each field
-    Object.entries(error.errors).forEach(([field, error]) => {
-        if(o.form_errors[field]) {
-            o.form_errors[field] = error.msg;
-        }
-    })
+  // Put general error from message
+  o.error = error.message;
 
-    return o;
-}
+  // Map error to each field from response to cacheable form error field
+  Object.values(error.errors).forEach((error: IFormErrorField) => {
+    if (o.form_errors.hasOwnProperty(error.param)) {
+      o.form_errors[error.param] = error.msg;
+    }
+  });
+
+  return o;
+};
+
+export const validateAllFormFields = (formGroup: FormGroup, cacheable: ICacheable<any>) => {
+  Object.keys(formGroup.controls).forEach((field) => {
+    const control = formGroup.get(field);
+    if (cacheable.form_errors[field]) {
+      control.setErrors({ incorrect: true });
+    }
+  });
+};
--- frontend/src/app/routes/landing/login/login.component.ts
+++ frontend/src/app/routes/landing/login/login.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
 
 import { AuthenticationService } from "../../../services/authentication.service";
-import { ICacheable } from 'src/app/app.interfaces';
-import { IUser } from '@eventi/interfaces';
-import { MyselfService } from 'src/app/services/myself.service';
-import { BaseAppService } from 'src/app/services/app.service';
-import { HttpErrorResponse } from '@angular/common/http';
-import { handleFormErrors } from 'src/app/_helpers/formErrorHandler';
-
-
-// const formErrorResponseHandler(formErrors:IFormErrorR)
-
+import { ICacheable } from "src/app/app.interfaces";
+import { IUser } from "@eventi/interfaces";
+import { MyselfService } from "src/app/services/myself.service";
+import { BaseAppService } from "src/app/services/app.service";
+import { HttpErrorResponse } from "@angular/common/http";
+import { handleFormErrors, validateAllFormFields } from "src/app/_helpers/formErrorHandler";
 
 @Component({
   selector: "app-login",
@@ -20,48 +21,60 @@ import { handleFormErrors } from 'src/app/_helpers/formErrorHandler';
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-  user:ICacheable<IUser> = {
+  user: ICacheable<IUser> = {
     data: null,
     error: "",
     loading: false,
     form_errors: {
-      email: "",
-      password: "",  
-    }
-  }
+      email_address: "",
+      password: "",
+    },
+  };
 
   returnUrl: string;
   loginForm: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
-    private baseAppService:BaseAppService,
+    private baseAppService: BaseAppService,
     private myselfService: MyselfService,
     private authService: AuthenticationService,
     private fb: FormBuilder
   ) {}
 
   ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      email_address: ["", [Validators.required]],
-      password: ["", [Validators.required]],
-      rememberMe: [false],
-    });
+    this.loginForm = this.fb.group(
+      {
+        email_address: ["", [Validators.required]],
+        password: ["", [Validators.required, Validators.minLength(6)]],
+        // rememberMe: [false],
+      }
+    );
 
     this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/";
   }
 
-  get email() { return this.loginForm.get("email_address") }
-  get password() { return this.loginForm.get("password") }
+  get email() {
+    return this.loginForm.get("email_address");
+  }
+  get password() {
+    return this.loginForm.get("password");
+  }
 
   submitHandler() {
     this.user.loading = true;
-    this.authService.login(this.loginForm.value)
-      .then(u => {
+    this.authService
+      .login(this.loginForm.value)
+      .then((u) => {
         this.myselfService.setUser(u);
-        this.baseAppService.navigateTo('/');
+        this.baseAppService.navigateTo("/");
       })
-      .catch((e:HttpErrorResponse) => this.user = handleFormErrors(this.user, e.error))
+      .catch(
+        (e: HttpErrorResponse) => {
+          this.user = handleFormErrors(this.user, e.error)
+          validateAllFormFields(this.loginForm, this.user) 
+        }
+      )
       .finally(() => this.user.loading = false);
   }
 }
