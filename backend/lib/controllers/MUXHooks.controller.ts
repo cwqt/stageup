ba7949da71788dbc068ba0e81b6a22469ca83d89
Cass <dev@cass.si>
2020-12-17 12:49:44 +0000

    fix merge issues from prev merge

--- backend/lib/controllers/MUXHooks.controller.ts
+++ backend/lib/controllers/MUXHooks.controller.ts
@@ -8,81 +8,100 @@ import { ErrorHandler } from '../common/errors';
 import { HTTP } from '@eventi/interfaces';
 import { RedisClient } from 'redis';
 import { MD5 } from 'object-hash';
+import { AuthStrategy } from '../authorisation';
+import { BaseArgs, IControllerEndpoint, BaseController } from '../common/controller';
+export default class MUXHooksController extends BaseController {
+  hookMap: { [index in MUXHook]?: (data: IMUXHookResponse<any>, dc: DataClient) => Promise<void> };
 
-const streamCreated = async (data: IMUXHookResponse<LiveStream>) => {
-  console.log(data);
-};
-
-const hookMap: { [index in MUXHook]?: (data: IMUXHookResponse<any>, dc: DataClient) => Promise<void> } = {
-  [MUXHook.StreamCreated]: streamCreated,
-};
-
-export const handleHook = async (req: Request, dc: DataClient) => {
-  try {
-    //https://github.com/muxinc/mux-node-sdk#verifying-webhook-signatures
-    const isValidHook = Webhooks.verifyHeader(
-      JSON.stringify(req.body),
-      req.headers['mux-signature'] as string,
-      config.MUX.HOOK_SIGNATURE
-    );
-
-    if (!isValidHook) throw new ErrorHandler(HTTP.BadRequest, 'Invalid MUX hook');
-  } catch (error) {
-    throw new ErrorHandler(HTTP.BadRequest, error.message);
+  constructor(...args: BaseArgs) {
+    super(...args);
+    this.hookMap = {
+      [MUXHook.StreamCreated]: this.streamCreated,
+    };
   }
 
-  // Is a valid hook & we should handle it
-  const data: IMUXHookResponse<any> = req.body;
+  async streamCreated(data: IMUXHookResponse<LiveStream>) {
+    console.log(data);
+  }
 
-  // TODO: At some point we'll want to add these hooks to a FIFO task queue and just respond with a 200
-  // for acknowledged handling, hook then handled by a separate micro-service
-  logger.http(`Received MUX hook: ${data.type}`);
+  validHookStrat(): AuthStrategy {
+    return async (req: Request): Promise<[boolean, {}, string?]> => {
+      try {
+        //https://github.com/muxinc/mux-node-sdk#verifying-webhook-signatures
+        const isValidHook = Webhooks.verifyHeader(
+          JSON.stringify(req.body),
+          req.headers['mux-signature'] as string,
+          config.MUX.HOOK_SIGNATURE
+        );
 
-  try {
-    // Check if hook has already been handled by looking in the Redis store
-    if (data.attempts.length > 0) {
-      if (await checkPreviouslyHandledHook(req.body, dc.redis)) {
-        logger.info(`Duplicate MUX hook`);
-        return;
+        if (!isValidHook) return [false, {}, 'Invalid MUX hook signature'];
+      } catch (error) {
+        return [false, {}, error.message];
       }
-    }
 
-    await (hookMap[data.type] || unsupportedHookHandler)(req.body, dc);
-    await setHookHandled(req.body, dc.redis);
-  } catch (error) {
-    throw new ErrorHandler(HTTP.ServerError, error);
+      return [true, {}];
+    };
   }
-};
 
-export const setHookHandled = async (data: IMUXHookResponse<any>, redis: RedisClient): Promise<void> => {
-  return new Promise((res, rej) => {
-    const hookId = `hook:${MD5(data.data)}`;
+  handleHook(): IControllerEndpoint<void> {
+    return {
+      authStrategies: [this.validHookStrat()],
+      controller: async (req: Request) => {
+        // Is a valid hook & we should handle it
+        const data: IMUXHookResponse<any> = req.body;
 
-    redis
-      .multi() //atomicity
-      .hmset(hookId, {
-        hookId: data.id,
-        objectId: data.object.id,
-        type: data.type,
-      })
-      .expire(hookId, 86400) //expire after 1 day
-      .exec((err, reply) => {
-        if (err) return rej(err);
-        res();
-      });
-  });
-};
+        // TODO: At some point we'll want to add these hooks to a FIFO task queue and just respond with a 200
+        // for acknowledged handling, hook then handled by a separate micro-service
+        logger.http(`Received MUX hook: ${data.type}`);
 
-export const checkPreviouslyHandledHook = async (data: IMUXHookResponse<any>, redis: RedisClient): Promise<boolean> => {
-  return new Promise((res, rej) => {
-    redis.hmget(`hook:${MD5(data.data)}`, 'hookId', (err, reply) => {
-      if (err) return rej(err);
-      if (reply[0]) return res(true);
-      return res(false);
+        try {
+          // Check if hook has already been handled by looking in the Redis store
+          if (data.attempts.length > 0) {
+            if (await this.checkPreviouslyHandledHook(req.body, this.dc.redis)) {
+              logger.info(`Duplicate MUX hook`);
+              return;
+            }
+          }
+
+          await (this.hookMap[data.type] || this.unsupportedHookHandler)(req.body, this.dc);
+          await this.setHookHandled(req.body, this.dc.redis);
+        } catch (error) {
+          throw new ErrorHandler(HTTP.ServerError, error);
+        }
+      },
+    };
+  }
+
+  setHookHandled = async (data: IMUXHookResponse<any>, redis: RedisClient): Promise<void> => {
+    return new Promise((res, rej) => {
+      const hookId = `hook:${MD5(data.data)}`;
+
+      redis
+        .multi() //atomicity
+        .hmset(hookId, {
+          hookId: data.id,
+          objectId: data.object.id,
+          type: data.type,
+        })
+        .expire(hookId, 86400) //expire after 1 day
+        .exec((err, reply) => {
+          if (err) return rej(err);
+          res();
+        });
     });
-  });
-};
+  };
 
-export const unsupportedHookHandler = async (data: IMUXHookResponse<any>, dc: DataClient) => {
-  logger.http(`Un-supported MUX hook: ${data.type}`);
-};
+  checkPreviouslyHandledHook = async (data: IMUXHookResponse<any>, redis: RedisClient): Promise<boolean> => {
+    return new Promise((res, rej) => {
+      redis.hmget(`hook:${MD5(data.data)}`, 'hookId', (err, reply) => {
+        if (err) return rej(err);
+        if (reply[0]) return res(true);
+        return res(false);
+      });
+    });
+  };
+
+  async unsupportedHookHandler(data: IMUXHookResponse<any>) {
+    logger.http(`Un-supported MUX hook: ${data.type}`);
+  }
+}
--- backend/lib/routes.ts
+++ backend/lib/routes.ts
@@ -4,17 +4,17 @@ import { DataClient } from './common/data';
 import Middlewares from './common/middleware';
 
 import {
-  IHost,
-  IUser,
-  IPerformanceStub as IPerfS,
-  IPerformance as IPerf,
-  IPerformanceHostInfo as IPHInfo,
-  IUserHostInfo as IUHInfo,
-  IPerformancePurchase as IPurchase,
-  IEnvelopedData as IE,
-  IPerformanceUserInfo as IPUInfo,
-  IMyself,
-} from '@eventi/interfaces';
+    IHost,
+    IUser,
+    IPerformanceStub as IPerfS,
+    IPerformance as IPerf,
+    IPerformanceHostInfo as IPHInfo, 
+    IUserHostInfo as IUHInfo,
+    IPerformancePurchase as IPurchase,
+    IEnvelopedData as IE,
+    IPerformanceUserInfo as IPUInfo,
+    IMyself,
+} from "@eventi/interfaces";
 
 import UserController from './controllers/User.controller';
 import HostController from './controllers/Host.controller';
@@ -26,55 +26,56 @@ import authorisation from './authorisation';
 /**
  * @description: Create a router, passing in the providers to be accessible to routes
  */
-export default (providers: DataClient): Router => {
-  const router = new Router(providers);
-  const mws = new Middlewares(providers);
+export default (providers:DataClient):Router => {
+const router = new Router(providers);
+const mws = new Middlewares(providers);
 
-  // USERS --------------------------------------------------------------------------------------------------------------
-  const Users = new UserController(providers, mws);
-  router.get<IMyself>('/myself', Users.readMyself());
-  router.post<IUser>('/users', Users.createUser());
-  router.post<void>('/users/logout', Users.logoutUser());
-  router.post<IUser>('/users/login', Users.loginUser());
-  router.get<IUser>('/users/@:username', Users.readUserByUsername()); // order matters
-  router.get<IUser>('/users/:uid', Users.readUserById());
-  router.put<IUser>('/users/:uid', Users.updateUser());
-  router.delete<void>('/users/:uid', Users.deleteUser());
-  router.get<IE<IHost, IUHInfo>>('/users/:uid/host', Users.getUserHost());
-  router.put<IUser>('/users/:uid/avatar', Users.updateUserAvatar());
-  router.put<void>('/users/:uid/password', Users.resetPassword());
-  // router.get    <IPurchase[]>         ("/users/:uid/purchases",                 Users.getPurchases);
-  // router.get    <IUserHostInfo>       ("/hosts/:hid/permissions",               Users.getUserHostPermissions);
-  router.get<void>('/feed', Users.readUserFeed());
+// USERS --------------------------------------------------------------------------------------------------------------
+const Users = new UserController(providers, mws);
+router.get    <IMyself>             ("/myself",                               Users.readMyself());
+router.post   <IUser>               ("/users",                                Users.createUser());
+router.post   <void>                ("/users/logout",                         Users.logoutUser());
+router.post   <IUser>               ("/users/login",                          Users.loginUser());
+router.get    <IUser>               ("/users/@:username",                     Users.readUserByUsername()); // order matters
+router.get    <IUser>               ("/users/:uid",                           Users.readUserById());
+router.put    <IUser>               ("/users/:uid",                           Users.updateUser());
+router.delete <void>                ("/users/:uid",                           Users.deleteUser());
+router.get    <IE<IHost, IUHInfo>>  ("/users/:uid/host",                      Users.getUserHost());
+router.put    <IUser>               ("/users/:uid/avatar",                    Users.updateUserAvatar());
+router.put    <void>                ("/users/:uid/password",                  Users.resetPassword());
+// router.get    <IPurchase[]>         ("/users/:uid/purchases",                 Users.getPurchases);
+// router.get    <IUserHostInfo>       ("/hosts/:hid/permissions",               Users.getUserHostPermissions);
+router.get    <void>                ("/feed",                                 Users.readUserFeed())
 
-  // HOSTS --------------------------------------------------------------------------------------------------------------
-  const Hosts = new HostController(providers, mws);
-  router.post<IHost>('/hosts', Hosts.createHost());
-  router.get<IUser[]>('/hosts/:hid/members', Hosts.getHostMembers());
-  // router.put    <IHost>               ("/hosts/:hid",                           Hosts.updateHost());
-  router.delete<void>('/hosts/:hid', Hosts.deleteHost());
-  // router.post   <IHost>               ("/hosts/:hid/membe",                     Hosts.addUser());
-  // router.delete <IHost>               ("/hosts/:hid/members",                   Hosts.removeUser());
-  // router.delete <IHost>               ("/hosts/:hid/members/:mid/permissions",  Hosts.alterMemberPermissions());
-  router.put<void>('/hosts/:hid/onboarding', Hosts.updateOnboarding());
+// HOSTS --------------------------------------------------------------------------------------------------------------
+const Hosts = new HostController(providers, mws);   
+router.post   <IHost>               ("/hosts",                                Hosts.createHost());
+router.get    <IUser[]>             ("/hosts/:hid/members",                   Hosts.getHostMembers());
+// router.put    <IHost>               ("/hosts/:hid",                           Hosts.updateHost());
+router.delete <void>                ("/hosts/:hid",                           Hosts.deleteHost());
+// router.post   <IHost>               ("/hosts/:hid/membe",                     Hosts.addUser());
+// router.delete <IHost>               ("/hosts/:hid/members",                   Hosts.removeUser());
+// router.delete <IHost>               ("/hosts/:hid/members/:mid/permissions",  Hosts.alterMemberPermissions());
+router.put<void>                    ("/hosts/:hid/onboarding",                Hosts.updateOnboarding());
 
-  // PERFORMANCES -------------------------------------------------------------------------------------------------------
-  const Perfs = new PerfController(providers, mws);
-  router.post<IPerf>('/performances', Perfs.createPerformance());
-  router.get<IE<IPerfS[], null>>('/performances', Perfs.getPerformances());
-  router.get<IE<IPerf, IPUInfo>>('/performances/:pid', Perfs.getPerformance());
-  router.get<IPHInfo>('/performances/:pid/host_info', Perfs.getPerformanceHostInfo());
-  router.post<void>('/performances/:pid/purchase', Perfs.purchase());
-  router.delete<void>('/performance/:pid', Perfs.deletePerformance());
 
-  // MUX HOOKS ----------------------------------------------------------------------------------------------------------
-  const MUXHooks = new MUXHooksController(providers, mws);
-  router.post<void>('/mux/hooks', MUXHooks.handleHook());
+// PERFORMANCES -------------------------------------------------------------------------------------------------------
+const Perfs = new PerfController(providers, mws);
+router.post   <IPerf>               ("/performances",                           Perfs.createPerformance());
+router.get    <IE<IPerfS[], null>>  ("/performances",                           Perfs.getPerformances());
+router.get    <IE<IPerf, IPUInfo>>  ("/performances/:pid",                      Perfs.getPerformance());
+router.get    <IPHInfo>             ("/performances/:pid/host_info",            Perfs.getPerformanceHostInfo());
+router.post   <void>                ("/performances/:pid/purchase",             Perfs.purchase());
+router.delete <void>                ("/performance/:pid",                       Perfs.deletePerformance());
 
-  // MISC ---------------------------------------------------------------------------------------------------------------
-  const Auth = new AuthController(providers, mws);
-  router.redirect('/auth/verify', Auth.verifyUserEmail());
-  router.get<string>('/ping', { authStrategies: [authorisation.none], controller: async (req: Request) => 'Pong!' });
+// MUX HOOKS ----------------------------------------------------------------------------------------------------------
+const MUXHooks = new MUXHooksController(providers, mws);
+router.post   <void>               ("/mux/hooks",                               MUXHooks.handleHook());
 
-  return router;
+// MISC ---------------------------------------------------------------------------------------------------------------
+const Auth =  new AuthController(providers, mws);
+router.redirect                    ("/auth/verify",                             Auth.verifyUserEmail());
+router.get    <string>             ("/ping",                                    { authStrategies: [authorisation.none], controller: async (req:Request) => "Pong!" });
+
+return router;
 };
