10a069823cb03e23be2634f4940f2918fae9136f
shreya <shreya2105@gmail.com>
2021-01-13 17:27:25 +0000

    Update performance-crud.story.ts

--- test/lib/stories/performances/performance-crud.story.ts
+++ test/lib/stories/performances/performance-crud.story.ts
@@ -4,6 +4,7 @@ import { expect } from 'chai';
 import { IHost, IPerformance, IUser } from '@eventi/interfaces';
 import { Stories } from '../../stories';
 import { environment, UserType } from '../../environment';
+import { performance } from 'perf_hooks';
 
 describe('As a user, I want to be able to do performance CRUD', async () => {
   let user: IUser;
@@ -11,10 +12,11 @@ describe('As a user, I want to be able to do performance CRUD', async () => {
   let perf: IPerformance;
 
   it('Should create a performance', async () => {
-    user = await Stories.actions.users.createUser(UserType.Client);
+    perf = await Stories.actions.performances.createPerformance(name);
 
-    expect(user).to.not.be.null;
-    expect(user.username).to.be.eq(environment.userCreationData[UserType.Client].username);
+    expect(perf).to.not.be.null;
+
+    expect(perf).to.be.eq(environment.userCreationData[UserType.Client].username);
   });
 
   it('Should get the newly created performance', async () => {});
