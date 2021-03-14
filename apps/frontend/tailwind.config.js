017d96d2df9b33e9ae12a89da4607fa136954fc3
Cass <dev@cass.si>
2021-03-14 22:56:20 +0000

    [Bug] Stop tailwind tree shaking grid span styles

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = (isProd) => ({
     content: [
       '**/*.html',
       '**/*.ts',
-    ]
+    ],
+    options: {
+      safelist: [
+        "col-span-1",
+        "col-span-2",
+        "col-span-3",
+        "col-span-4",
+        "col-span-5",
+        "col-span-6",
+        "col-span-7",
+        "col-span-8",
+        "col-span-9",
+        "col-span-10",
+        "col-span-11",
+        "col-span-12"
+      ],
+    }
   },
   darkMode: false, // or 'media' or 'class'
   theme: {},
