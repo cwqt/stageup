9ed9ec788b5b559cbb119534086f711ca1764a0d
Cass <dev@cass.si>
2021-04-25 14:54:17 +0100

    release

--- ship.config.js
+++ ship.config.js
@@ -6,9 +6,17 @@ module.exports = {
     packagesToPublish: [],
     updateDependencies: true
   },
+  afterPublish: ({ exec }) => {
+    exec(`git config --global user.email "dev@cass.si"`);
+    exec(`git config --global user.name "Cass"`);
+
+    exec('git checkout master');
+    exec('git merge dev');
+    exec('git push origin master');
+  },
   mergeStrategy: {
     toReleaseBranch: {
-      next: 'master',
-    },
-  },
+      next: 'master'
+    }
+  }
 };
