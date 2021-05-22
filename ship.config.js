4af6316c0d00bd5613e83bbbd3c75cd886e9e65f
Cass <dev@cass.si>
2021-05-22 16:38:29 +0100

    Update ship.config.js

--- ship.config.js
+++ ship.config.js
@@ -5,18 +5,5 @@ module.exports = {
     packagesToBump: [],
     packagesToPublish: [],
     updateDependencies: true
-  },
-  afterPublish: ({ exec }) => {
-    exec(`git config --global user.email "dev@cass.si"`);
-    exec(`git config --global user.name "Cass"`);
-
-    exec('git checkout master');
-    exec('git merge dev');
-    exec('git push origin master');
-  },
-  mergeStrategy: {
-    toReleaseBranch: {
-      next: 'master'
-    }
   }
 };
