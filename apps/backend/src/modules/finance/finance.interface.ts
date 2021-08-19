db6a441c96727f09b754f3c5abbb75325c5380d7
Ben Bennett <ben@Bens-Mac-mini.local>
2021-08-19 14:04:47 +0100

    Fixed spelling mistake in IProcessRefund

--- apps/backend/src/modules/finance/finance.interface.ts
+++ apps/backend/src/modules/finance/finance.interface.ts
@@ -4,5 +4,5 @@ export interface IProcessRefund {
   host_id: string;
   invoice_ids: string[];
   bulk_refund_data: IBulkRefund;
-  send_inititaion_emails: boolean;
+  send_initiation_emails: boolean;
 }
