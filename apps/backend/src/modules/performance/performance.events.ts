fa79d2d49cae7bd480414195b8a06956a4435658
Ben Bennett <ben@Bens-Mac-mini.local>
2021-08-19 15:29:36 +0100

    Corrected typo in initiation

--- apps/backend/src/modules/performance/performance.events.ts
+++ apps/backend/src/modules/performance/performance.events.ts
@@ -156,7 +156,7 @@ export class PerformanceEvents extends ModuleEvents {
             bulk_refund_reason: BulkRefundReason.PerformanceDeletedAutoRefund,
             bulk_refund_detail: null
           },
-          send_inititaion_emails: false
+          send_initiation_emails: false
         },
         ct.__meta.locale
       );
