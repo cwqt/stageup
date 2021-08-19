1beccd179f3292efeb1ceff4194ae71af05fdb60
Ben Bennett <ben@Bens-Mac-mini.local>
2021-08-19 12:09:23 +0100

    changes

--- libs/interfaces/src/performances/performance.interface.ts
+++ libs/interfaces/src/performances/performance.interface.ts
@@ -48,13 +48,13 @@ export interface IPerformanceStub {
   description?: RichText; // description of performance
   rating_count: number; // Total scores accumulated
   rating_total: number; // Number of ratings
-  premiere_datetime?: number; // when the performance is ready to be streamed
   views: number; // total user view count
   like_count: number; // total user likes
   created_at: number;
   assets?: IAssetStub[];
   thumbnail: string;
   status: PerformanceStatus;
+  publicity_period: { start: number; end: number }; // unix timestamps
 }
 
 export interface IFeedPerformanceStub extends IPerformanceStub {
@@ -66,7 +66,6 @@ export interface IPerformance extends IPerformanceStub {
   genre: Genre;
   tickets: ITicketStub[];
   assets: IAssetStub[];
-  publicity_period: { start: number; end: number }; // unix timestamps
 }
 
 // Interface for additional client information regarding the performance.
