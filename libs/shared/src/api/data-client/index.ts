38ddd3d51fe26c4ed0187046b058cd3f78b6f762
George Webberley <georgewebberley@googlemail.com>
2021-09-13 16:37:36 +0200

    Delete local tunnel from providers map before instantiation (when not Env.IS_LOCAL)

--- libs/shared/src/api/data-client/index.ts
+++ libs/shared/src/api/data-client/index.ts
@@ -1,7 +1,8 @@
 import { timestamp } from '@core/helpers';
 import Container, { Token } from 'typedi';
 import { Logger } from './providers/logging.provider';
-import { LOGGING_PROVIDER } from './tokens';
+import { LOGGING_PROVIDER, HTTP_TUNNEL_PROVIDER } from './tokens';
+import Env from '@backend/env';
 
 /**
  * DataClient: Object with methods for maintaining all providers
@@ -28,6 +29,8 @@ export type ProviderMap = Map<Token<any>, Provider>;
 // Creates the DataClient object from an object of DataProviders
 const connect = async <T extends ProviderMap>(map: T): Promise<T> => {
   const log = Container.get(LOGGING_PROVIDER);
+
+  if (!Env.IS_LOCAL) map.delete(HTTP_TUNNEL_PROVIDER);
 
   for await (const [token, provider] of map.entries()) {
     // Show the message now, so if any fields are missing we know to what provider
