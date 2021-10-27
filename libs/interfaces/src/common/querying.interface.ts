61d90f1d3226de3336c6526eb633e1b4b34de37f
Ubadah Atta <ubadahatta12@gmail.com>
2021-10-27 15:06:07 +0100

    SU-1096 Adds hard-in status

--- libs/interfaces/src/common/querying.interface.ts
+++ libs/interfaces/src/common/querying.interface.ts
@@ -112,5 +112,6 @@ export type DateFilter = FilterQuery<FilterCode.Date, DateFilterOperator, [numbe
 
 export enum OptStatus {
   'hard-out' = 'Opted-Out',
+  'hard-in' = 'Opted-In',
   'soft-in' = 'Opted-In',
 }
