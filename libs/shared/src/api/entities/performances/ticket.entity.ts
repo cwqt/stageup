aa4c517464d7ad0b037583e035d3c1dfe048dc5f
Ubadah Atta <ubadahatta12@gmail.com>
2021-11-08 16:02:36 +0000

    SU-1082 Updates default dono_pegs /deploy

--- libs/shared/src/api/entities/performances/ticket.entity.ts
+++ libs/shared/src/api/entities/performances/ticket.entity.ts
@@ -36,7 +36,7 @@ export class Ticket extends BaseEntity implements ITicket {
   @Column('enum', { enum: CurrencyCode }) currency: CurrencyCode;
   @Column('enum', { enum: TicketType }) type: TicketType;
   // @Column('enum', { enum: TicketFees }) fees: TicketFees;
-  @Column('varchar', { array: true ,default: []}) dono_pegs: DonoPeg[];
+  @Column('varchar', { array: true, default: []}) dono_pegs: DonoPeg[];
 
   @DeleteDateColumn({ type: 'timestamptz' }) deleted_at?: Date; // soft delete
   @ManyToOne(() => Performance, perf => perf.tickets) performance: Performance;
