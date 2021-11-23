839cb1b5bbf50a0eadc6c8af03d28e03a18e1a64
kissillesgergely <kissigergely@gmail.com>
2021-11-23 17:08:14 +0000

    ci(SU-1051): modifies which ip ranges redis can use /deploy

--- terraform/modules/redis/main.tf
+++ terraform/modules/redis/main.tf
@@ -41,7 +41,13 @@ resource "google_compute_router_nat" "nat" {
   router                             = google_compute_router.router.name
   region                             = google_compute_router.router.region
   nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  # source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  subnetwork {
+    name                    = var.subnet.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
 
   log_config {
     enable = true
