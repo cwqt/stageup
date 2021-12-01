9afe393256c994fdcbe81d39f83668db697c5840
kissillesgergely <33480912+kissillesgergely@users.noreply.github.com>
2021-12-01 17:23:39 +0000

    ci(SU-1051): CIDR range assignments for feature-branch deploys (#325)
    merge: ci(SU-1051): changes the Github actions and the Terraform files to handle CIDR ranges dynamically
    
    also moves the api-key from the query string into a custom header

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,7 +70,7 @@ data "google_sql_database_instance" "postgres" {
 resource "google_compute_subnetwork" "subnet" {
   provider      = google-beta
   name          = "subnet-${local.name}"
-  ip_cidr_range = "10.2.0.0/28"
+  ip_cidr_range = replace(var.cidr_range, "\"", "")
   region        = local.region
   network       = data.google_compute_network.vpc.id
 }
@@ -102,7 +102,8 @@ module "redis" {
   name    = local.name
   region  = local.region
   network = data.google_compute_network.vpc.name
-  subnet  = google_compute_subnetwork.subnet.name
+  subnetname  = google_compute_subnetwork.subnet.name
+  subnetlink = google_compute_subnetwork.subnet.self_link
 }
 
 # allow the cloud run backend to connect to the db
