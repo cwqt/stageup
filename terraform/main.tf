82493def1124d152166b4d220f7fa9bde7889ed4
kissillesgergely <kissigergely@gmail.com>
2021-12-02 08:44:15 +0000

    bug(SU-1168): applies the maximum possible timeout for the cloud run resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -204,7 +204,8 @@ module "frontend" {
   port                = 80
   # this timeout needs to be this long to enable SSE to work properly
   # 14400s = 4h which should be sufficient in case of a long stream as well
-  timeout             = 14400
+  # Apparently the highest value you can define is 3600
+  timeout             = 3600
   max_instances       = var.core == "prod" ? 2 : 1
   min_instances       = 1
   map_domains         = [local.load_balancer_host]
