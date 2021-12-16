4d86006f0b5671afd6ab21b45a4a07bc4c06a97c
Gergely Kiss-Illes <kissigergely@gmail.com>
2021-12-16 13:55:23 +0000

    bug(SU-1207-3): changes timeouts again

--- terraform/main.tf
+++ terraform/main.tf
@@ -154,8 +154,6 @@ module "backend" {
   region        = local.region
   image         = "eu.gcr.io/${var.gcp_project_id}/core-backend:${local.name}-latest"
   vpc_connector = google_vpc_access_connector.vpc_connector.name
-    # this timeout needs to be this long to enable SSE to work properly
-  timeout_seconds = 1200
 
   env = {
     NODE_ENV                    = local.NODE_ENV
@@ -206,7 +204,7 @@ module "frontend" {
   allow_public_access = true
   port                = 80
   # this timeout needs to be this long to enable SSE to work properly
-  timeout_seconds     = 1200
+  timeout             = 1200
   max_instances       = var.core == "prod" ? 2 : 1
   min_instances       = 1
   map_domains         = [local.load_balancer_host]
